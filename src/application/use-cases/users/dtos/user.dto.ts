40fb3de7f013d3d26eb929813f3bbebf6184f8bf
Fernando Juarez <fhereduardo90@gmail.com>
2021-03-04 19:09:08 -0600

    refacor: inherit from domain user

--- src/application/use-cases/users/dtos/user.dto.ts
+++ src/application/use-cases/users/dtos/user.dto.ts
@@ -1,21 +1,3 @@
-import { Expose, Exclude, Transform } from 'class-transformer'
 import { User } from '../../../../domain/user'
 
-@Exclude()
-export class UserDto implements Omit<User, 'password' | 'createdAt'> {
-  @Expose()
-  readonly id: string
-
-  @Expose()
-  readonly firstName: string
-
-  @Expose()
-  readonly lastName: string
-
-  @Expose()
-  readonly email: string
-
-  @Expose()
-  @Transform(({ value }) => value?.toISOString())
-  readonly createdAt: Date
-}
+export class UserDto extends User {}
